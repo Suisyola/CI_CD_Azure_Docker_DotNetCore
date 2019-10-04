d15fbb6ccd81b73ec36a65b976124e8ac72d981c
unknown <tics_zz@hotmail.com>
2019-10-04 23:02:59 +0800

    Added new ASP .Net Core MVC Web App

---
+++ CI_CD_WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CI_CD_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
